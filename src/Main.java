37fa0593651e86ee5ddf5fff5198db8dd57dc93d
XCLHove <2959169303@qq.com>
2023-03-04 21:48:33 +0800

    1.fix Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] argls) {
+    public static void main(String[] args) {
         //创建窗口对象
         Window window = new Window();
         //设置窗口可见
